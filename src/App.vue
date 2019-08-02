85c0e5d9672af281c6380dcbfbaddd7d88f079bf
Gen1usG <812487021@qq.com>
2019-08-02 21:24:40 +0800

    submit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,97 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container">
+      <div>
+        <div class="wrapper">
+          <Cell @clickchess="changeN(0,$event)" :clicktime="n" :win="winner"></Cell>
+          <Cell @clickchess="changeN(1,$event)" :clicktime="n" :win="winner"></Cell>
+          <Cell @clickchess="changeN(2,$event)" :clicktime="n" :win="winner"></Cell>
+        </div>
+        <div class="wrapper">
+          <Cell @clickchess="changeN(3,$event)" :clicktime="n" :win="winner"></Cell>
+          <Cell @clickchess="changeN(4,$event)" :clicktime="n" :win="winner"></Cell>
+          <Cell @clickchess="changeN(5,$event)" :clicktime="n" :win="winner"></Cell>
+        </div>
+        <div class="wrapper">
+          <Cell @clickchess="changeN(6,$event)" :clicktime="n" :win="winner"></Cell>
+          <Cell @clickchess="changeN(7,$event)" :clicktime="n" :win="winner"></Cell>
+          <Cell @clickchess="changeN(8,$event)" :clicktime="n" :win="winner"></Cell>
+        </div>
+      </div>
+      <div>{{winner}}</div>
+    </div>
+    
+ 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cell from "./components/cell";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Cell
+  },
+  data() {
+    return {
+      n: 0,
+      result: [[null, null, null], [null, null, null], [null, null, null]],
+      winner: "胜负未分"
+    };
+  },
+  methods: {
+    changeN(id, chess) {
+      if (this.winner === "胜负未分") {
+        this.n++;
+        this.result[Math.floor(id / 3)][id % 3] = chess;
+        this.judege();
+      }
+    },
+
+    judege() {
+      for (let i = 0; i <= 2; i++) {
+        if (
+          this.result[i][0] != null &&
+          this.result[i][0] == this.result[i][1] &&
+          this.result[i][1] == this.result[i][2]
+        ) {
+          this.winner = `${this.result[i][0]}'胜利'`;
+        }
+      }
+      for (let j = 0; j <= 2; j++) {
+        if (
+          this.result[0][j] != null &&
+          this.result[0][j] == this.result[1][j] &&
+          this.result[1][j] == this.result[2][j]
+        ) {
+          this.winner = `${this.result[0][j]}'胜利'`;
+        }
+      }
+      if (
+        this.result[0][0] == this.result[1][1] &&
+        this.result[0][0] == this.result[2][2] &&
+        this.result[0][0] != null
+      ) {
+        this.winner = `${this.result[0][0]}'胜利'`;
+      } else if (
+        this.result[0][2] == this.result[1][1] &&
+        this.result[0][2] == this.result[2][0] &&
+        this.result[0][2] != null
+      ) {
+        this.winner = `${this.result[0][2]}'胜利'`;
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.wrapper {
+  display: flex;
+}
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: column;
 }
 </style>
---
+++ src/components/cell.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div class="chesstable" @~click="clickInTable">
+      <div class="chess" v-if="a">{{text}}</div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      text: null,
+      a: false
+    };
+  },
+  props: ["clicktime", "win"],
+  methods: {
+    clickInTable() {
+      if (this.win === "胜负未分") {
+        if (this.text !== null) {
+          return;
+        }
+        this.a = true;
+        if (this.clicktime % 2 == 0) {
+          this.text = "x";
+        } else {
+          this.text = "o";
+        }
+        this.$emit("clickchess", this.text);
+      }
+    }
+  }
+};
+</script>
+
+<style>
+.chesstable {
+  width: 100px;
+  height: 100px;
+  border: 1px solid #111;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.chess {
+  font-size: 80px;
+}
+</style>
+
