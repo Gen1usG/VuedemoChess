b00c60931f305326ae1e2019aa84d1d59820ebca
Gen1usG <812487021@qq.com>
2019-10-19 16:29:13 +0800

    new

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/chess/dist/'
-      : '/'
-  }
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/VuedemoChess/dist/'
+    : '/'
+}
